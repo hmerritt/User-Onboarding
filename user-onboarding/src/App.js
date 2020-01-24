5f6bb740ec8d93689b46e52c917f43bd1293b948
Harry Merritt <harry@merritt.es>
2020-01-24 00:51:15 +0000

    Created form component

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import UserForm from './components/Form/UserForm';
+import logo from './logo.svg';
+import './App.css';
+
+function App()
+{
+    return (
+        <div className="App">
+            <UserForm />
+        </div>
+    );
+}
+
+export default App;
