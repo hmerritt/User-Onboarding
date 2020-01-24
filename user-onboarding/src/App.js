f07364a6b221d394159740b2652040622b4879e5
Harry Merritt <harry@merritt.es>
2020-01-24 02:59:39 +0000

    Created User list component to dislpay all users

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -1,16 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserForm from './components/UserForm/UserForm';
+import UserList from './components/UserList/UserList';
 import logo from './logo.svg';
 import './App.css';
 
 function App()
 {
+
+    //  Users array
+    const [users, setUsers] = useState([
+        {
+            name: 'John Doe',
+            email: 'john@doe.com',
+            password: 'johndoe77'
+        }
+    ]);
+
     return (
         <div className="App container">
             <header>
                 <h1>User Onboarding</h1>
             </header>
-            <UserForm />
+
+            {/*  Add user Form  */}
+            <UserForm users={users} setUsers={setUsers} />
+
+            {/*  Users  */}
+            <UserList users={users} />
         </div>
     );
 }
